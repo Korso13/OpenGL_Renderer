97043ade5e2e3f9bb170058960a072fe90a49daf
Korsakov Timur <101480776+Korso13@users.noreply.github.com>
2023-08-23 22:51:33 +0400

    Moved shader utilities funcs to separate file. Added index buffer-based render of rectangle.

--- OpenGLTester/OpenGLTester.cpp
+++ OpenGLTester/OpenGLTester.cpp
@@ -1,72 +1,9 @@
 #include <cstdio>
-#include <fstream>
-#include <iostream>
-#include <string>
-
 #include "../external/include/GLEW/glew.h"
 #include "../external/include/GLFW/glfw3.h"
+#include "source/Utilities/ShaderUtilities.h"
 
-#define TRIANGLE_VERTBUF_SIZE 6
-
-static GLuint CompileShader(const std::string& source, GLuint type)
-{
-    GLuint shader_id = glCreateShader(type);
-    const char* src = source.c_str();
-    glShaderSource(shader_id, 1, &src, nullptr);
-    glCompileShader(shader_id);
-
-    //error handling:
-    int result;
-    glGetShaderiv(shader_id, GL_COMPILE_STATUS, &result);
-    if(result == GL_FALSE)
-    {
-        int error_length;
-        glGetShaderiv(shader_id, GL_INFO_LOG_LENGTH, &error_length);
-        char* errMsg = (char*)alloca(error_length * sizeof(char));
-        glGetShaderInfoLog(shader_id, error_length, &error_length, errMsg);
-
-        std::cout << "Failed to compile " << (type == GL_VERTEX_SHADER ? "vertex " : "fragment ") << "shader!" << std::endl;
-        std::cout << errMsg << std::endl;
-
-        glDeleteShader(shader_id);
-        return 0;
-    }
-    
-    return shader_id;
-}
-
-void GetSrcCode(const std::string& _path, std::string& _srcString)
-{
-    std::ifstream ShaderSrcFile;
-    ShaderSrcFile.open(_path);
-
-    std::string line;
-    while(!ShaderSrcFile.eof())
-    {
-        line.clear();
-        std::getline(ShaderSrcFile, line);
-        _srcString += line + "\n";
-    }
-
-    ShaderSrcFile.close();
-}
-
-static GLuint CreateShader(const std::string& _vertexShader, const std::string& _fragmentShader)
-{
-    GLuint program = glCreateProgram();
-    GLuint vs = CompileShader(_vertexShader, GL_VERTEX_SHADER);
-    GLuint fs = CompileShader(_fragmentShader, GL_FRAGMENT_SHADER);
-
-    glAttachShader(program, vs);
-    glAttachShader(program, fs);
-    glLinkProgram(program);
-    glValidateProgram(program);
-
-    glDeleteShader(vs);
-    glDeleteShader(fs);
-    
-    return program;
-}
+#define TRIANGLE_VERTBUF_SIZE 8
 
 int main(void)
 {
@@ -98,15 +35,13 @@ int main(void)
         std::cout << "OpenGL ver: " << glGetString(GL_VERSION) << std::endl;
     }
 
+    //creating vertex buffer:
     float vertex1Verts[TRIANGLE_VERTBUF_SIZE] =     //array of vertices
         {
             -0.5f, -0.5,
-            -0.2f,  0.3f,
-             0.5f, -0.5f
-        };
-    int vertex1Indices[TRIANGLE_VERTBUF_SIZE/2] =
-        {
-            0, 1, 2
+             0.5f, -0.5f,
+            0.5f, 0.5f,
+            -0.5f, 0.5f
         };
     
     unsigned int vertex1Buffer; //buffer ID for triangle
@@ -114,45 +49,40 @@ int main(void)
     glBindBuffer(GL_ARRAY_BUFFER, vertex1Buffer);
     glBufferData(GL_ARRAY_BUFFER, sizeof(float) * TRIANGLE_VERTBUF_SIZE, vertex1Verts, GL_STATIC_DRAW);
 
+    //creating layout:
     glEnableVertexAttribArray(0);
     glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0);
 
+    //creating index buffer:
+    unsigned int rect1Indices[3 * 2] =
+        {
+            0, 1, 2,
+            0, 2, 3
+        };
+    unsigned int index1Buffer; //buffer ID for vertex indices
+    glGenBuffers(1, &index1Buffer);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, index1Buffer);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(unsigned int) * 3 * 2, rect1Indices, GL_STATIC_DRAW);
+    
     //generating shader
     //first, we extract source code:
     std::string vertex_shader;
     std::string fragment_shader;
-    GetSrcCode("shaders/stdVS.vs", vertex_shader);
-    GetSrcCode("shaders/stdFS.fs", fragment_shader);
+    shaderUtils::GetSrcCode("shaders/stdVS.vs", vertex_shader);
+    shaderUtils::GetSrcCode("shaders/stdFS.fs", fragment_shader);
 
     //now we create shader program
-    const GLuint shader_program = CreateShader(vertex_shader, fragment_shader);
+    const GLuint shader_program = shaderUtils::CreateShader(vertex_shader, fragment_shader);
     glUseProgram(shader_program);
 
-    bool bReverse = false;
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
 
-        glDrawArrays(GL_TRIANGLES, 0, 3);
-        //glDrawElements(GL_TRIANGLES, 3, GL_FLOAT, &vertex1Indices);
-        
-        //adding movement to one of triangle's vertices
-        glBufferData(GL_ARRAY_BUFFER, sizeof(float) * TRIANGLE_VERTBUF_SIZE, vertex1Verts, GL_STATIC_DRAW);
-        if(!bReverse)
-        {
-            vertex1Verts[2] += 0.01f;
-        }
-        else
-        {
-            vertex1Verts[2] -= 0.01f;
-        }
-
-        if(vertex1Verts[2] >= 0.8f || vertex1Verts[2] <= -0.8f)
-        {
-            bReverse = !bReverse;
-        }
+        //glDrawArrays(GL_TRIANGLES, 0, 3);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
         
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
---
+++ OpenGLTester/source/Utilities/ShaderUtilities.h
@@ -0,0 +1,72 @@
+#pragma once
+#include <GLEW/glew.h>
+#include <iostream>
+#include <fstream>
+#include <string>
+
+namespace shaderUtils
+{
+    //compiles provided string with source code into specified type of shader. Returns 0 on error and prints it in console
+    GLuint CompileShader(const std::string& source, GLuint type)
+    {
+        GLuint shader_id = glCreateShader(type);
+        const char* src = source.c_str();
+        glShaderSource(shader_id, 1, &src, nullptr);
+        glCompileShader(shader_id);
+
+        //error handling:
+        int result;
+        glGetShaderiv(shader_id, GL_COMPILE_STATUS, &result);
+        if(result == GL_FALSE)
+        {
+            int error_length;
+            glGetShaderiv(shader_id, GL_INFO_LOG_LENGTH, &error_length);
+            char* errMsg = (char*)alloca(error_length * sizeof(char));
+            glGetShaderInfoLog(shader_id, error_length, &error_length, errMsg);
+
+            std::cout << "Failed to compile " << (type == GL_VERTEX_SHADER ? "vertex " : "fragment ") << "shader!" << std::endl;
+            std::cout << errMsg << std::endl;
+
+            glDeleteShader(shader_id);
+            return 0;
+        }
+    
+        return shader_id;
+    }
+
+    //reads source code from provided path to shader source code and writes it into provided string (_srcString)
+    static void GetSrcCode(const std::string& _path, std::string& _srcString)
+    {
+        std::ifstream ShaderSrcFile;
+        ShaderSrcFile.open(_path);
+
+        std::string line;
+        while(!ShaderSrcFile.eof())
+        {
+            line.clear();
+            std::getline(ShaderSrcFile, line);
+            _srcString += line + "\n";
+        }
+
+        ShaderSrcFile.close();
+    }
+
+    //creates shader program build using provided source code for vertex and fragment shaders. Returns program id
+    static GLuint CreateShader(const std::string& _vertexShader, const std::string& _fragmentShader)
+    {
+        GLuint program = glCreateProgram();
+        GLuint vs = shaderUtils::CompileShader(_vertexShader, GL_VERTEX_SHADER);
+        GLuint fs = shaderUtils::CompileShader(_fragmentShader, GL_FRAGMENT_SHADER);
+
+        glAttachShader(program, vs);
+        glAttachShader(program, fs);
+        glLinkProgram(program);
+        glValidateProgram(program);
+
+        glDeleteShader(vs);
+        glDeleteShader(fs);
+    
+        return program;
+    }
+
+}
